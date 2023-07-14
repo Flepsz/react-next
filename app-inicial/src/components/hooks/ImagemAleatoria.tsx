58e9078909ae2653272011e960df7a136232b8aa
Flepsz <luisfelipelfsp3@gmail.com>
2023-07-14 12:59:47 -0300

    Updt

---
+++ app-inicial/src/components/hooks/ImagemAleatoria.tsx
@@ -0,0 +1,15 @@
+export default function ImagemAleatoria() {
+    function renderizarBotao() {
+        return (
+            <button className="bg-blue-500 px-4">
+                teste
+            </button>
+        )
+    }
+
+    return (
+        <div>
+            Imgram
+        </div>
+    )
+}
